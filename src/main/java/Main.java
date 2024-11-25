267ad5d81ea6e80614e35721c121a1df3ffaf9b7
ankitkumarsamota121 <ankitkumarsamota121@gmail.com>
2024-11-25 23:32:09 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import helpers.Client;
@@ -15,7 +15,7 @@ public class Main {
 
         int port = 6379;
         ServerSocket serverSocket;
-        Map<String, String> datastore = new HashMap<>();
+        Map<String, String> datastore = new ConcurrentHashMap<>();
         try {
             serverSocket = new ServerSocket(port);
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
